d9c2719fa84c99c7f8b5decf24a7a174a8ac5ca2
Giuseppe Rosa <giusepper11@gmail.com>
2019-01-23 00:10:27 -0200

    Adicionando bootstrap

--- gestao_rh/urls.py
+++ gestao_rh/urls.py
@@ -1,9 +1,8 @@
-
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('apps.core.urls')),
-    path('funcionario/', include('apps.funcionarios.urls'))
+    path('funcionarios/', include('apps.funcionarios.urls'))
 ]
--- manage.py
+++ manage.py
@@ -12,5 +12,5 @@ if __name__ == '__main__':
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
-    print('http://127.0.0.1:9001/')
+    print('http://127.0.0.1:8000/')
     execute_from_command_line(sys.argv)
